e3b16ddb902933dbe65de9232280815d59520467
tcw <1483674736@qq.com>
2019-07-20 17:25:32 +0800

    完成了后台管理界面的数据总览部分

---
+++ src/test/java/daoTest.java
@@ -0,0 +1,27 @@
+import com.web.core.mapper.UserMapper;
+import com.web.core.pojo.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext-dao.xml")
+public class daoTest {
+
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testDao(){
+        List<User> users = userMapper.queryAll(0, 5);
+        for (User u:users){
+            System.out.println("--->"+u.toString());
+        }
+    }
+}
